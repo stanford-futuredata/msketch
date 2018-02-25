08d502b5fe4dc67e948bc9f505c67a79e3f4d908
Jialin Ding <djialin09@gmail.com>
2018-02-24 18:04:28 -0800

    cascade timing

--- javamsketch/src/main/java/SlidingWindowBench.java
+++ javamsketch/src/main/java/SlidingWindowBench.java
@@ -24,6 +24,14 @@ public class SlidingWindowBench {
         long createTime = 0;
         long mergeTime = 0;
         long queryTime = 0;
+        long cascadeTime = 0;
+        long markovTime = 0;
+        long momentTime = 0;
+        long maxentTime = 0;
+        int numEnterCascade = 0;
+        int numAfterNaiveCheck = 0;
+        int numAfterMarkovBound = 0;
+        int numAfterMomentBound = 0;
     }
 
     public SlidingWindowBench(String confFile) throws IOException {
@@ -86,12 +94,50 @@ public class SlidingWindowBench {
         System.out.format("Avg create time: %f\n", timing.createTime / (1.e9 * trialsDone));
         System.out.format("Avg merge time: %f\n", timing.mergeTime / (1.e9 * trialsDone));
         System.out.format("Avg query time: %f\n", timing.queryTime / (1.e9 * trialsDone));
+        int numEnterCascade = timing.numEnterCascade / trialsDone;
+        int prunedByNaive = (timing.numEnterCascade - timing.numAfterNaiveCheck) / trialsDone;
+        int prunedByMarkov = (timing.numAfterNaiveCheck - timing.numAfterMarkovBound) / trialsDone;
+        int prunedByMoments = (timing.numAfterMarkovBound - timing.numAfterMomentBound) / trialsDone;
+        double overallThroughput = timing.numEnterCascade / (timing.cascadeTime / 1.e9);
+        double naiveThroughput = timing.numEnterCascade / ((timing.cascadeTime - timing.markovTime - timing.momentTime - timing.maxentTime) / 1.e9);
+        double markovThroughput = timing.numAfterNaiveCheck / (timing.markovTime / 1.e9);
+        double momentThroughput = timing.numAfterMarkovBound / (timing.momentTime / 1.e9);
+        double maxentThroughput = timing.numAfterMomentBound / (timing.maxentTime / 1.e9);
+        double naiveTime = (timing.cascadeTime - timing.markovTime - timing.momentTime - timing.maxentTime) / (double)timing.cascadeTime;
+        double markovTime = timing.markovTime / (double)timing.cascadeTime;
+        double momentTime = timing.momentTime / (double)timing.cascadeTime;
+        double maxentTime = timing.maxentTime / (double)timing.cascadeTime;
+        System.out.format(
+                        "Entered cascade: %d (%f qps)\n\t" +
+                        "Pruned by naive checks: %d (%f) (%f qps) (%f)\n\t" +
+                        "Pruned by Markov bounds: %d (%f) (%f qps) (%f)\n\t" +
+                        "Pruned by moment bounds: %d (%f) (%f qps) (%f)\n\t" +
+                        "Reached maxent: %d (%f) (%f qps) (%f)\n",
+                numEnterCascade / trialsDone, overallThroughput,
+                prunedByNaive, prunedByNaive / (double)numEnterCascade, naiveThroughput, naiveTime,
+                prunedByMarkov, prunedByMarkov / (double)numEnterCascade, markovThroughput, markovTime,
+                prunedByMoments, prunedByMoments / (double)numEnterCascade, momentThroughput, momentTime,
+                timing.numAfterMomentBound / trialsDone, timing.numAfterMomentBound / (double)timing.numEnterCascade, maxentThroughput, maxentTime);
         System.out.format("Num alerts: %d\n\n", numAlerts);
 
         Map<String, String> result = new HashMap<String, String>();
         result.put("simple", String.format("%b", cascadeStages[0]));
         result.put("markov", String.format("%b", cascadeStages[1]));
         result.put("racz", String.format("%b", cascadeStages[2]));
+        result.put("simple_hit", String.format("%d", timing.numEnterCascade / trialsDone));
+        result.put("markov_hit", String.format("%d", timing.numAfterNaiveCheck / trialsDone));
+        result.put("racz_hit", String.format("%d", timing.numAfterMarkovBound / trialsDone));
+        result.put("maxent_hit", String.format("%d", timing.numAfterMomentBound / trialsDone));
+        result.put("simple_throughput", String.format("%f", naiveThroughput));
+        result.put("markov_throughput", String.format("%f", markovThroughput));
+        result.put("racz_throughput", String.format("%f", momentThroughput));
+        result.put("maxent_throughput", String.format("%f", maxentThroughput));
+        result.put("overall_throughput", String.format("%f", overallThroughput));
+        result.put("simple_time", String.format("%f", naiveTime));
+        result.put("markov_time", String.format("%f", markovTime));
+        result.put("racz_time", String.format("%f", momentTime));
+        result.put("maxent_time", String.format("%f", maxentTime));
+
         result.put("avg_runtime", String.format("%f", timeElapsed / (1.e9 * trialsDone)));
         result.put("avg_createtime", String.format("%f", timing.createTime / (1.e9 * trialsDone)));
         result.put("avg_mergetime", String.format("%f", timing.mergeTime / (1.e9 * trialsDone)));
@@ -152,7 +198,7 @@ public class SlidingWindowBench {
             paneLogMins.remove();
             paneLogMaxs.remove();
             timing.createTime += System.nanoTime() - createStart;
-            
+
             mergeStart = System.nanoTime();
             double[] windowTotalSums = sketch.getTotalSums();
             double[] paneTotalSums = paneSketch.getTotalSums();
@@ -188,6 +234,15 @@ public class SlidingWindowBench {
             alerts.add(alerter.checkAlert(sketch));
             timing.queryTime += System.nanoTime() - queryStart;
         }
+
+        timing.cascadeTime += alerter.actionTime;
+        timing.markovTime += alerter.markovBoundTime;
+        timing.momentTime += alerter.momentBoundTime;
+        timing.maxentTime += alerter.maxentTime;
+        timing.numEnterCascade += alerter.numEnterCascade;
+        timing.numAfterNaiveCheck += alerter.numAfterNaiveCheck;
+        timing.numAfterMarkovBound += alerter.numAfterMarkovBound;
+        timing.numAfterMomentBound += alerter.numAfterMomentBound;
 
         return alerts;
     }
