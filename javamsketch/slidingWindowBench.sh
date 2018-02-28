04bc0910e342cf04136a12257b81ce8b9919999c
Jialin Ding <djialin09@gmail.com>
2018-02-28 00:59:27 -0800

    actual config on future2

--- javamsketch/slidingWindowBench.sh
+++ javamsketch/slidingWindowBench.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-java -Xmx8g -Xms8g -cp target/java-msketch-1.0-SNAPSHOT.jar:$(cat cp.txt) SlidingWindowBench $@
+java -Xmx64g -Xms64g -cp target/java-msketch-1.0-SNAPSHOT.jar:$(cat cp.txt) SlidingWindowBench $@
