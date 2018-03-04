d1d53f055739a49dbcbbdb6d2214d01e4f56f5d1
Edward Gan <edgan8@gmail.com>
2018-03-04 13:14:06 -0800

    cleanup

--- mscript/build.sh
+++
@@ -1,3 +0,0 @@
-cd mlib && \
-cmake . && \
-make
--- mscript/tests/test_simple.py
+++
@@ -1,6 +0,0 @@
-import unittest
-from .context import msketch
-
-class TestSimple(unittest.TestCase):
-    def test_simple(self):
-        self.assertEqual(1, 1)
