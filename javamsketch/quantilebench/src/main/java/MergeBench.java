418e84abf4bf837fd492781573bdf155a4abf5b4
Jialin Ding <djialin09@gmail.com>
2018-05-22 22:33:04 -0700

    resolve merge bench conflicts

--- javamsketch/quantilebench/src/main/java/MergeBench.java
+++ javamsketch/quantilebench/src/main/java/MergeBench.java
@@ -14,7 +14,6 @@ public class MergeBench {
     private int columnIdx;
     private int cellSize;
     private List<Double> cellFractions;
-    private int numDuplications;
 
     private Map<String, List<Double>> methods;
     private List<Double> quantiles;
@@ -33,7 +32,6 @@ public class MergeBench {
         cellSize = conf.get("cellSize");
         List<Double> defaultCellFractions = Arrays.asList(1.0);
         cellFractions = conf.get("cellFractions", defaultCellFractions);
-        numDuplications = conf.get("numDuplications", 1);
 
         methods = conf.get("methods");
         quantiles = conf.get("quantiles");
