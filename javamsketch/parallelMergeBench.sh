ef7c0504469d16b2db130b6d4010bb21c719c0cc
Jialin Ding <djialin09@gmail.com>
2018-05-29 00:00:07 -0700

    updated parallel merge and aria configs

--- javamsketch/parallelMergeBench.sh
+++ javamsketch/parallelMergeBench.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-java -Xmx10g -Xms10g -cp quantilebench/target/quantile-bench-1.0-SNAPSHOT.jar:$(cat quantilebench/cp.txt) \
+java -Xmx50g -Xms50g -cp quantilebench/target/quantile-bench-1.0-SNAPSHOT.jar:$(cat quantilebench/cp.txt) \
 ParallelMergeBench $@
